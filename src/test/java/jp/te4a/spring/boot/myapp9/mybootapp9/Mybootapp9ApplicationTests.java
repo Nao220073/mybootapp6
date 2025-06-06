2bfca16a9b01e589e0046c06c520631b4aefa4cb
Nao220073 <te220073@gmail.com>
2025-06-06 14:40:45 +0900

    6/6 commit

--- src/test/java/jp/te4a/spring/boot/myapp9/mybootapp9/Mybootapp9ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp9/mybootapp9/Mybootapp9ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybootapp8ApplicationTests {
+class Mybootapp9ApplicationTests {
 
 	@Test
 	void contextLoads() {
